b08bd8638520d36b45c14f154d31182f175e050a
Aswinsuresh <Aswin.Sureshbabu@triosstudent.com>
2024-08-26 19:58:01 -0400

    gave a new UI for Skills section

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -28,37 +28,53 @@ export const Skills = () => {
     }
   };
 
+  const skillsData = [
+    {
+      img: meter1,
+      title: "Web Development",
+      description: "Creating responsive and interactive websites using modern web technologies."
+    },
+    {
+      img: meter2,
+      title: "Web Design",
+      description: "Designing visually appealing and user-friendly web interfaces and modern technologies."
+    },
+    {
+      img: meter3,
+      title: "UI/UX Design",
+      description: "Focusing on user experience and interface design to ensure user satisfaction."
+    },
+    {
+      img: meter1,
+      title: "Software Engineering",
+      description: "Applying engineering principles to design, develop, and maintain software."
+    }
+  ];
+
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Web Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>UI/UX Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry's standard dummy text.</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                {skillsData.map((skill, index) => (
+                  <div className="item" key={index}>
+                    <div className="skill-box">
+                      <img src={skill.img} alt={`${skill.title} Icon`} />
+                      <h5>{skill.title}</h5>
+                      <p>{skill.description}</p>
                     </div>
-                </div>
+                  </div>
+                ))}
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Background" />
     </section>
-  )
+  );
 }
